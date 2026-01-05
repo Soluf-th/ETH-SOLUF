9b72d5e0004e7f1587ba36192f125e9e9664607a
So-luf <132810911+Soluf-th@users.noreply.github.com>
2026-01-06 02:49:19 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -17,4 +17,18 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
+const config = createConfig({
+  ssr: true, // Enable this if your dapp uses server-side rendering.
+  chains: [mainnet, linea, lineaSepolia],
+  connectors: [
+    metaMask({
+      infuraAPIKey: process.env.NEXT_PUBLIC_INFURA_API_KEY!,
+    }),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [linea.id]: http(),
+    [lineaSepolia.id]: http(),
+  },
+});
